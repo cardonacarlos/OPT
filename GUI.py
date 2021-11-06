6f532d5bf89c6196ba549d086c63cb16feb7a825
cardonacarlos <91919255+cardonacarlos@users.noreply.github.com>
2021-11-06 17:54:42 -0500

    edits

--- GUI.py
+++ GUI.py
@@ -11,12 +11,9 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
+#hello button function
 def write_slogan():
     print("Welcome to the application!")
-
-
-
-# def write_value():
 
 #button to quit the app
 quitbutton = tk.Button(frame,
@@ -29,9 +26,7 @@ slogan = tk.Button(frame,
                    command=write_slogan)
 slogan.pack(side=tk.LEFT)
 
-
-
-#function to find a company's SEC filing from ticker
+#function to find a company's SEC report from ticker
 def hyperlink():
     url = 'https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/000032019320000096/aapl-20200926.htm'
     webbrowser.register('chrome',
@@ -40,29 +35,26 @@ def hyperlink():
                             "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
     webbrowser.get('chrome').open_new(url)
 
-#SEC filing launcher button
+#SEC filing launcher button on frame
 webpage = tk.Button(frame,
                     text="Report",
                     command=hyperlink)
 webpage.pack(side=tk.LEFT)
 
-
-
+#function that obtains user input
 def user_input():
     global input
     global string
-    string = input.get()
+    string = input.get().lower()
     print(string)
     return string
 
-#input (ticker)
+#input button (ticker)
 input = tk.Entry(root)
 input.pack(side=tk.BOTTOM)
 input.focus_set()
 inputbutton = tk.Button(root, text='Get Input', command=user_input)
 inputbutton.pack(side=tk.LEFT)
-
-
 
 # Reads ticker to cik file
 tick = pd.read_csv("ticker.txt", sep="\t")
@@ -74,7 +66,7 @@ def tick_to_cik(ticker):
         if ticker == tick['Ticker'].iloc[i]:
             return str(tick['CIK'].iloc[i]).zfill(10)
 
-
+#list of items that we want to observe
 items = """
 Assets
 CommonStockSharesAuthorized
@@ -98,16 +90,18 @@ WeightedAverageNumberOfSharesOutstandingBasic
 NetIncomeLoss
 """
 items = items.split()
-
 string = input.get()
 
 
-# function that accesses SEC Edgar API on command
+# function that accesses SEC Edgar API on command and returns facts
 def company_facts():
+    #from inputted string obtain request the sec json page
     response = requests.get("https://data.sec.gov/api/xbrl/companyfacts/CIK"  + tick_to_cik(string) + '.json').text
     data = json.loads(response)
     metric_dict = {}
     for key, val in data['facts']['us-gaap'].items():
+        date = max(ten_k['filed'])
+        ten_k = ten_k[(ten_k['filed'] == date) & (ten_k['form'] == '10-K') & (ten_k['frame'].isna())]
         unit_df = pd.DataFrame()
         for unit, unit_val in val['units'].items():
             val_data = val['units'][unit]
@@ -119,8 +113,6 @@ def company_facts():
     for item in items:
         metric = metric_dict[item]
         ten_k = metric[metric['form'] == '10-K']
-        date = max(ten_k['filed'])
-        ten_k = ten_k[(ten_k['filed'] == date) & (ten_k['form'] == '10-K') & (ten_k['frame'].isna())]
 
         #if the value for the requested metric is not null, then print
         #else find the latest metric for it available
@@ -134,7 +126,6 @@ def company_facts():
         else:
             print(item + ": Value: ")
             print(ten_k.iloc[0]['val'])
-
 
 
 def testprint():
