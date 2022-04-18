6e31e139c98081e540825758b505234ae44d3cf9
Carlos Cardona Amparan <cardonac@uchicago.edu>
2022-04-18 15:05:55 -0500

    updates

--- GUI.py
+++ GUI.py
@@ -16,14 +16,15 @@ from pptx.dml.color import RGBColor
 from pptx.enum.dml import MSO_THEME_COLOR
 from pptx.enum.text import PP_ALIGN
 
+
+from logic import *
+
+
 #makes the frame
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-#hello button function
-def write_slogan():
-    print("Welcome to the application!")
 
 #button to quit the app
 quitbutton = tk.Button(root,
@@ -39,14 +40,14 @@ slogan = tk.Button(root,
 slogan.pack(side=tk.LEFT, anchor=tk.NW)
 slogan.focus_set()
 
-#function to find a company's SEC report from ticker
-def hyperlink():
-    url = 'https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/000032019320000096/aapl-20200926.htm'
-    webbrowser.register('chrome',
-                        None,
-                        webbrowser.BackgroundBrowser(
-                            "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
-    webbrowser.get('chrome').open_new(url)
+# #function to find a company's SEC report from ticker
+# def hyperlink():
+#     url = 'https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/000032019320000096/aapl-20200926.htm'
+#     webbrowser.register('chrome',
+#                         None,
+#                         webbrowser.BackgroundBrowser(
+#                             "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+#     webbrowser.get('chrome').open_new(url)
 
 #SEC filing launcher button on frame
 webpage = tk.Button(root,
@@ -54,13 +55,6 @@ webpage = tk.Button(root,
                     command=hyperlink)
 webpage.pack(side=tk.LEFT, anchor=tk.NW)
 
-#function that obtains user input
-def user_input():
-    global input
-    global string
-    string = input.get().lower()
-    print(string)
-    return string
 
 #input button (for ticker)
 input = tk.Entry(root)
@@ -69,15 +63,15 @@ input.focus_set()
 inputbutton = tk.Button(root, text='Get Input', command=user_input)
 inputbutton.pack(side=tk.LEFT, anchor=tk.NW)
 
-# Reads ticker to cik file
-tick = pd.read_csv("ticker.txt", sep="\t")
+# # Reads ticker to cik file
+# tick = pd.read_csv("ticker.txt", sep="\t")
 
 
-# function that converts ticker to cik
-def tick_to_cik(ticker):
-    for i in range(len(tick['Ticker'])):
-        if ticker == tick['Ticker'].iloc[i]:
-            return str(tick['CIK'].iloc[i]).zfill(10)
+# # function that converts ticker to cik
+# def tick_to_cik(ticker):
+#     for i in range(len(tick['Ticker'])):
+#         if ticker == tick['Ticker'].iloc[i]:
+#             return str(tick['CIK'].iloc[i]).zfill(10)
 
 #list of items that we want to observe
 items = """
@@ -115,78 +109,6 @@ info_label = tk.Label(root,
  )
 info_label.pack(side=tk.RIGHT)
 
-# function that accesses SEC Edgar API on command and returns facts
-def company_facts():
-
-    #declare global info variable
-    global info
-    info = string.upper() + " Company Facts\n"
-
-    #url for api
-    url = "https://data.sec.gov/api/xbrl/companyfacts/CIK"  + tick_to_cik(string) + ".json"
-
-
-    #header for accessing API
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
-    'From': 'ccardona2000@gmail.com' 
-    }
-
-    #api accessing
-    response = requests.get(url, headers=headers)
-
-    #ensure that connect works
-    if not response.ok:
-        print('Retry')
-    else:
-        #load json data
-        response = response.text
-        data = json.loads(response)
-
-        #dictionary for wanted metric
-        metric_dict = {}
-
-
-        for key, val in data['facts']['us-gaap'].items():
-            unit_df = pd.DataFrame()
-
-            for unit, unit_val in val['units'].items():
-                val_data = val['units'][unit]
-                val_data_df = pd.DataFrame(unit_val)
-                val_data_df['unit'] = unit
-                
-                
-                # deprecated warning
-                # unit_df = unit_df.append(val_data_df)
-                unit_df = pd.concat([unit_df, val_data_df])
-                metric_dict[key] = unit_df
-
-        for item in items:
-            metric = metric_dict[item]
-            ten_k = metric[metric['form'] == '10-K']
-            date = max(ten_k['filed'])
-            ten_k = ten_k[(ten_k['filed'] == date) & (ten_k['form'] == '10-K') & (ten_k['frame'].isna())]
-
-            #if the value for the requested metric is not null, then print
-            #else find the latest metric for it available
-            if ten_k['val'].empty:
-                ten_k = metric
-                date = max(ten_k['filed'])
-                ten_k = ten_k[(ten_k['filed']==date)]   
-                print(item + ": Value: ")
-                print(ten_k.iloc[0]['val'])
-                info = info + " ".join(re.sub(r"([A-Z])", r" \1", str(item)).split()) + ": " + "{:,}".format(ten_k.iloc[0]['val']) + "\n"
-                info_label.config(text=info)
-
-            else:
-                print(item + ": Value: ")
-                print(ten_k.iloc[0]['val'])
-                info = info + " ".join(re.sub(r"([A-Z])", r" \1", str(item)).split()) + ": " + "{:,}".format(ten_k.iloc[0]['val']) +"\n"
-                info_label.config(text=info)
-    print("\n===================================================")
-    print(info)
- 
-
 
 
 
@@ -202,68 +124,6 @@ financial_info.pack(side=tk.LEFT, anchor=tk.NW)
 
 # response = requests.get('https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json').text
 
-
-
-
-#command that makes powerpoint from financial information
-def make_powerpoint():
-
-    #split financial info into array
-    split_info = info.split("\n")
-
-    #initialize powerpoint
-    prs = Presentation()
-    title_slide_layout = prs.slide_layouts[0]
-    slide = prs.slides.add_slide(title_slide_layout)
-    title = slide.shapes.title
-    subtitle = slide.placeholders[1]
-
-    title.text = "Company Facts for " + string.upper()
-
-    #make shape for each info
-    shapes = slide.shapes
-    left = top = height = Inches(0.5)
-    width = Inches(0.7)
-    for text in range(len(split_info)):
-
-        #shape configuration
-        shape = shapes.add_shape(
-        MSO_SHAPE.RECTANGLE, left, top, width, height
-        )
-
-        #shape filling
-        fill = shape.fill 
-        fill.solid()
-        fill.fore_color.rgb = RGBColor(255,255,255)
-
-        #shape outline
-        line = shape.line
-        line.color.rgb = RGBColor(0,0,0)
-
-        #shape text
-        text_frame = shape.text_frame
-        text_frame.word_wrap = True
-
-        p = text_frame.paragraphs[0]
-        p.alignment = PP_ALIGN.LEFT
-        run = p.add_run()
-        run.text = split_info[text]
-
-        #shape text font
-        font = run.font
-        font.name = 'Arial Narrow'
-        font.size = Pt(6)
-        font.color.rgb = RGBColor(0,0,0)
-        left = left + width
-
-    pptx_name = string.upper() + ".pptx"
-    prs.save(pptx_name)
-
-    #open powerpoint file that was just created
-
-    cwd = os.getcwd()
-    path = cwd + "\\" + pptx_name
-    os.startfile(path)
 
 
 
--- logic.py
+++ logic.py
@@ -1,6 +1,16 @@
+import requests
+import json
+
+
 
 # print welcome to test button feature
 import string
+
+#header for accessing API
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+    'From': 'ccardona2000@gmail.com' 
+}
 
 
 def write_slogan():
@@ -11,13 +21,177 @@ def hyperlink(CIK):
     url = 'https://www.sec.gov/edgar/browse/?CIK=' + CIK
     webbrowser.open_new_tab(url)
 
+# declare ticker string variable
+ticker = tk.StringVar()
+
 # get typed user input
 def user_input():
-    global input
-    global string
-    s = input.get().lower()
+    s = ticker.get()
     print(s)
     return s
 
 # read ticker to cik file
 # refer to link https://www.sec.gov/files/company_tickers.json
+def tick_to_cik(ticker):
+    url = 'https://www.sec.gov/files/company_tickers.json'
+    
+    response = requests.get(url, headers=headers)
+    
+    if not response.ok:
+        print('Retrying...')
+    else:
+        data = response.json()
+        for tick in data.values():
+            if tick['ticker'].lower() == ticker.lower():
+                print(tick['title'])
+                return tick['cik_str']
+            
+#list of items that we want to observe
+items = """
+Assets
+CommonStockSharesAuthorized
+CommonStockSharesIssued
+CommonStockSharesOutstanding
+CommonStockDividendsPerShareDeclared
+CostOfGoodsAndServicesSold
+Dividends
+EarningsPerShareDiluted
+Liabilities
+LiabilitiesAndStockholdersEquity
+LongTermDebt
+LongTermDebtCurrent
+LongTermDebtMaturitiesRepaymentsOfPrincipalAfterYearFive
+OperatingExpenses
+OperatingIncomeLoss
+PreferredStockSharesAuthorized
+Revenues
+StockholdersEquity
+WeightedAverageNumberOfSharesOutstandingBasic
+NetIncomeLoss
+"""
+# items = items.split()
+# string = input.get()
+
+# global info
+# info = ""
+
+
+# access SEC Edgar API and return facts
+def company_facts():
+    
+    info = string.upper() + " Company Facts\n"
+    
+    url = "https://data.sec.gov/api/xbrl/companyfacts/CIK"  + tick_to_cik(string) + ".json"
+    
+    response = requests.get(url, headers=headers)
+    
+    #ensure that connect works
+    if not response.ok:
+        print('Retry')
+    else:
+        #load json data
+        response = response.text
+        data = json.loads(response)
+
+        #dictionary for wanted metric
+        metric_dict = {}
+
+
+        for key, val in data['facts']['us-gaap'].items():
+            unit_df = pd.DataFrame()
+
+            for unit, unit_val in val['units'].items():
+                val_data = val['units'][unit]
+                val_data_df = pd.DataFrame(unit_val)
+                val_data_df['unit'] = unit
+                
+                
+                # deprecated warning
+                # unit_df = unit_df.append(val_data_df)
+                unit_df = pd.concat([unit_df, val_data_df])
+                metric_dict[key] = unit_df
+
+        for item in items:
+            metric = metric_dict[item]
+            ten_k = metric[metric['form'] == '10-K']
+            date = max(ten_k['filed'])
+            ten_k = ten_k[(ten_k['filed'] == date) & (ten_k['form'] == '10-K') & (ten_k['frame'].isna())]
+
+            #if the value for the requested metric is not null, then print
+            #else find the latest metric for it available
+            if ten_k['val'].empty:
+                ten_k = metric
+                date = max(ten_k['filed'])
+                ten_k = ten_k[(ten_k['filed']==date)]   
+                print(item + ": Value: ")
+                print(ten_k.iloc[0]['val'])
+                info = info + " ".join(re.sub(r"([A-Z])", r" \1", str(item)).split()) + ": " + "{:,}".format(ten_k.iloc[0]['val']) + "\n"
+                info_label.config(text=info)
+
+            else:
+                print(item + ": Value: ")
+                print(ten_k.iloc[0]['val'])
+                info = info + " ".join(re.sub(r"([A-Z])", r" \1", str(item)).split()) + ": " + "{:,}".format(ten_k.iloc[0]['val']) +"\n"
+                info_label.config(text=info)
+    print("\n===================================================")
+    print(info)
+    
+#command that makes powerpoint from financial information
+def make_powerpoint():
+
+    #split financial info into array
+    split_info = info.split("\n")
+
+    #initialize powerpoint
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+
+    title.text = "Company Facts for " + string.upper()
+
+    #make shape for each info
+    shapes = slide.shapes
+    left = top = height = Inches(0.5)
+    width = Inches(0.7)
+    for text in range(len(split_info)):
+
+        #shape configuration
+        shape = shapes.add_shape(
+        MSO_SHAPE.RECTANGLE, left, top, width, height
+        )
+
+        #shape filling
+        fill = shape.fill 
+        fill.solid()
+        fill.fore_color.rgb = RGBColor(255,255,255)
+
+        #shape outline
+        line = shape.line
+        line.color.rgb = RGBColor(0,0,0)
+
+        #shape text
+        text_frame = shape.text_frame
+        text_frame.word_wrap = True
+
+        p = text_frame.paragraphs[0]
+        p.alignment = PP_ALIGN.LEFT
+        run = p.add_run()
+        run.text = split_info[text]
+
+        #shape text font
+        font = run.font
+        font.name = 'Arial Narrow'
+        font.size = Pt(6)
+        font.color.rgb = RGBColor(0,0,0)
+        left = left + width
+
+    pptx_name = string.upper() + ".pptx"
+    prs.save(pptx_name)
+
+    #open powerpoint file that was just created
+
+    cwd = os.getcwd()
+    path = cwd + "\\" + pptx_name
+    os.startfile(path)
